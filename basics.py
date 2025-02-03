8d2903b1beaa09185b313dd0b6d206dc82718ca8
Jahnavi NRI <130889778+Janu2331@users.noreply.github.com>
2025-02-03 11:36:00 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,14 @@
+#List of the marks
+marks=[85,92,65,88]
+#Find the highest mark
+highestmark=max(92)
+print(f"Highest mark:{highestmark}")
+#values Greater than 80
+greaterthan80=[mark for mark in marks if mark>80]
+print(f"marks greater than 80:{greater than 80}")
+#values greater than 90
+greaterthan90=[mark for mark in marks if marks>90]
+print(f"marks greater than 90:{greater than 90}")
+#values between 60&90
+between 60&90=[mark for mark in marks if 60<=mark<=90]
+print(f"marks between 60&90: (between60&90)")
